18a13555dfd4e138bfaeb60945d78265c05b4f0c
7rcksh@gmail.com <7rcksh@gmail.com>
2019-10-10 22:24:19 +0530

    Added Search and MaxHeapift Top in Q2.

--- A6_2.c
+++ A6_2.c
@@ -18,7 +18,6 @@ void maxHeapifyBottom(int pos, int *heap, int num)
 
   return;
 }
-
 void max_HeapifyTop(int i, int n, int *arr)
 {
   if (i > n || 2 * i > n) {
@@ -58,66 +57,4 @@ int search(int *heap, int num, int size)
   }
 
   return -1;
-}
-
-int main()
-{
-  int x = 1;
-  int capacity = 2;
-  int num;
-  int i;
-  int temp;
-  int size = 1; // Will start inserting from position 1. and it will show the current elements of heap
-  int *heap = calloc(capacity, sizeof(int));
-  while (x != 4) {
-    printf("1. Insert the element\n");
-    printf("2. Delete the element\n");
-    printf("3. Display the element\n");
-    printf("4. Quit\n");
-    scanf("%d", &x);
-    switch (x) {
-      case 1:
-            scanf("%d", &num);
-            if (capacity == size) {
-              capacity = 2 * capacity;
-              heap = realloc(heap, capacity * sizeof(int));
-            }
-            maxHeapifyBottom(size, heap, num);
-            size++;
-            printf("Inserted\n");
-
-            break;
-      case 2:
-            scanf("%d", &num);
-            int pos = search(heap, num, size);
-            if (pos == -1) {
-              printf("%d Not Found\n", num);
-            } else {
-              temp = *(heap+size-1);
-              *(heap+size-1) = num;
-              *(heap+pos) = temp;
-              size--;
-              max_HeapifyTop(pos, size - 1, heap);
-              printf("Deleted\n");
-            }
-
-            break;
-      case 3:
-            if (size == 1) {
-              printf("Empty Heap");
-            }
-            for (i = 1; i < size; i++) {
-                printf("%d ", *(heap+i));
-            }
-            printf("\n");
-
-            break;
-      case 4:
-            exit(0);
-      default:
-              break;
-    }
-  }
-  
-  return 0;
 }
